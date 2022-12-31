f37d14fd5477ae0f26bef3c556b16b491273843d
Holger <holger.steffens@gmail.com>
2022-12-31 14:53:58 +0100

    convert to int/float

--- mqtt_sensors.py
+++ mqtt_sensors.py
@@ -83,6 +83,13 @@ class MqttDevice:
     def set_states(self, states_dict):
         for entity, value in states_dict.items():
             if entity in self._entities:
+                if value.isnumeric():
+                    value = int(value)
+                else:
+                    try:
+                        value = float(value)
+                    except:
+                        pass
                 if value != self._entities[entity]["value"]:
                     self._entities[entity]["value"] = value
                     self._entities[entity]["value_updated"] = True
