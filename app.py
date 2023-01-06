3ecd14fe39b6f507dc83a448f05b9ccadd57373c
Holger <holger.steffens@gmail.com>
2023-01-06 22:14:50 +0100

    main app implementation started

--- app.py
+++ app.py
@@ -1,28 +1,75 @@
-import logging, time, yaml
+import logging, math, threading, time
+from mqtt_device import MqttDevice, YamlInterface
 from wallbox import Wallbox
 
 SETTINGS = 'settings.yaml'
+ENTITIES = 'entities.yaml'
 
 logging.basicConfig(format='%(asctime)s | %(levelname)-8s | %(funcName)s() %(filename)s line=%(lineno)s | %(message)s',
                     level=logging.INFO)
 
+class CaptureTimer:
+    def __init__(self, interval, function):
+        self.__interval = interval
+        self.__function = function
+        self.__start_timer()
+        
+    def update_interval(self, interval):
+        self.__interval = interval
+        self.__timer.cancel()
+        self.__timer.join()
+        self.__start_timer()
+        
+    def __start_timer(self):
+        now = time.time()
+        sleep_time = math.ceil(now / self.__interval) * self.__interval - now        
+        self.__timer = threading.Timer(sleep_time, self.__timer_expired)
+        self.__timer.start()  
+    
+    def __timer_expired(self):
+        self.__function()     # execute timer function
+        self.__start_timer()  # schedule the next timer
+        
 
 if __name__ == "__main__":
-    with open(SETTINGS, 'r') as f:
-        settings = yaml.safe_load(f)
+    yaml_settings = YamlInterface(SETTINGS)
+    settings = yaml_settings.load()
+
+    entities = YamlInterface(ENTITIES).load()
+    
+    def on_message_callback(entity, message):
+        logging.info(f"{entity}, {message}")
+        device.set_states({entity: message})
+        device.publish_updates()  # send confirmation to homeassistant 
+    
+    device = MqttDevice(hostname=settings['mqtt']['hostname'], 
+                        port=settings['mqtt']['port'], 
+                        devicename=settings["devicename"], 
+                        client_id=settings['client_id'],
+                        entities=entities,
+                        on_message_callback=on_message_callback)    
     
     wb = Wallbox(port=settings["modbus"]["PORT"],
                  bus_id=settings["modbus"]["BUS_ID"],
                  max_read_attempts=settings["modbus"]["MAX_READ_ATTEMPTS"])
     
+    def do_capture():
+        pass
+    
+    def after_capture():
+        """Callback function executed after wallbox capture to process the return data.
+        """
+        pass
+    
+    
     try:
         while True:
-            task = {"func": "capture", "callback": logging.info}
-            wb.task_queue.put_nowait(task)
-            time.sleep(settings["update_interval"])
+            time.sleep(1)
     
     except KeyboardInterrupt:
         pass
     
-    logging.info("exiting main")
     wb.exit()
+    device.exit()
+    logging.info("exiting main")
+    
