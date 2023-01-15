b771c932d345a59515b92a02df3fd223126758f9
Holger <holger.steffens@gmail.com>
2023-01-15 18:55:18 +0100

    bugfix wd

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ SECRETS = 'secrets.yaml'
 wd = os.path.dirname(__file__)
 log_path = os.path.join(wd, "logging.txt")
 logging.basicConfig(format='%(asctime)s | %(levelname)-8s | %(funcName)s() %(filename)s line=%(lineno)s | %(message)s',
-                    handlers=[logging.FileHandler(wd), logging.StreamHandler(),],
+                    handlers=[logging.FileHandler(log_path), logging.StreamHandler(),],
                     level=logging.INFO)
 
 class CaptureTimer:
